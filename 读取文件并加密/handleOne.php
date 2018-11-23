3b121001824ad42cc0aca8e6b9a70aec29710c6b
LAP <2300820209@qq.com>
2018-11-23 08:01:25 +0800

    “完整版密“完整base64加密”

--- 读取文件并加密/handleOne.php
+++ 读取文件并加密/handleOne.php
@@ -1,10 +1,44 @@
 <?php
-if (isset($_POST['file_base64'])){
-    var_dump($_POST);
-    $file_base64 = $_POST['file_base64'];
-    $file_base64 = preg_replace('/data:.*;base64,/i', '', $file_base64);
-    $file_base64 = base64_decode($file_base64);
-    var_dump($file_base64);
-    file_put_contents('./file.save', $file_base64);
-} 
+// var_dump($_POST);
+// var_dump($_FILES);
+
+$data = file_get_contents($_FILES["file"]["tmp_name"]);
+
+var_dump($data);
+
+$data_decode = preg_replace('/data:.*;base64,/i', '',base64_decode($data));
+$data_decode = preg_replace('/data:.*;base64,/i', '', base64_decode($data_decode));
+var_dump($data_decode);
+
+file_put_contents('./file.txt',$data_decode );
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//<?php
+//if (isset($_POST['file_base64'])){
+  //  $file_base64 = $_POST['file_base64'];
+   // $file_base64 = preg_replace('/data:.*;base64,/i', '', $file_base64);
+    //$file_base64 = base64_decode($file_base64);
+    //$file_base64 = preg_replace('/data:.*;base64,/i', '', $file_base64);
+    //$file_base64 = base64_decode($file_base64);
+    //var_dump($file_base64);
+    //file_put_contents('./file.save', $file_base64);
+//} 
+//?>
+
+
