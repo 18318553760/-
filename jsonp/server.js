a0372ed168f9775313344243094d83691e95104a
LAP <2300820209@qq.com>
2019-01-25 21:04:33 +0800

    jsonp

---
+++ jsonp/server.js
@@ -0,0 +1,9 @@
+let express = require('express')
+let app = express()
+app.get('/say', function(req, res) {
+  let { wd, callback } = req.query
+  console.log(wd) // Iloveyou
+  console.log(callback) // show
+  res.end(`${callback}('I like you!')`)
+})
+app.listen(3000)
